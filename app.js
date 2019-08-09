6c5ed2860b8cff2fde7db27d0fa7ebdd254ea1f9
QiGaoCode <45082230+QiGaoCode@users.noreply.github.com>
2019-08-08 20:10:42 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Budget controller
+var budgetController = (function(){
+    
+    // some code
+    
+})();
+
+
+// UI controller
+var UIController = (function(){
+
+    // some code
+    
+})();
+
+
+// Global app controller
+var controller = (function(budgetCtrl,UICtrl){
+    
+    // some code
+    
+})(budgetController,UIController);
